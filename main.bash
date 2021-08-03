275f925cf1e1e99b53fc2f6f0fb084e44d887714
curlyanku <88360545+curlyanku@users.noreply.github.com>
2021-08-03 09:46:58 +0530

    Add files via upload

---
+++ main.bash
@@ -0,0 +1,11 @@
+# 
+# Welcome to GDB Online.
+# GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+# C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+# Code, Compile, Run and Debug online from anywhere in world.
+# 
+# 
+
+echo "My name is Ankita N Banubakode";
+echo "my mail id : ankitabanuko79994@gmail.com.";
+echo "my hobbies: cooking, and exploring new things";
